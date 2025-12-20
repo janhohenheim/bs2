a8ab7c69a8798949f04d09f7edd83b9195ce7009
Jan Hohenheim <jan@hohenheim.ch>
2025-12-20 21:18:56 +0100

    Implement copying

--- crates/manager/src/main.rs
+++ crates/manager/src/main.rs
@@ -58,8 +58,9 @@ fn is_setup_done() -> bool {
 fn run_setup(app: slint::Weak<App>) -> impl FnMut() {
     move || {
         let app = app.unwrap();
-        slint::spawn_local(async move {
-            let cs2_path = PathBuf::from(app.global::<Bs2Config>().get_cs2_path().as_str());
+        let cs2_path = PathBuf::from(app.global::<Bs2Config>().get_cs2_path().as_str());
+        let app = app.as_weak();
+        std::thread::spawn(move || {
             let paths_to_copy = ["core", "_toolsettings", "thirdpartylegalnotices.txt", "bin"];
 
             let sources = paths_to_copy.map(|p| cs2_path.join("game").join(p));
@@ -72,6 +73,8 @@ fn run_setup(app: slint::Weak<App>) -> impl FnMut() {
             if let Err(e) = fs::create_dir_all(&dest) {
                 error!("Failed to create {canon_dest}: {e:?}");
             }
+            let app_inner = app.clone();
+            let mut total_bytes = 0;
             let result = copy_items_with_progress(
                 &sources,
                 &dest,
@@ -86,25 +89,58 @@ fn run_setup(app: slint::Weak<App>) -> impl FnMut() {
                         &process.file_name
                     };
                     let path = cs2_path.join("game").join(name);
+                    total_bytes = process.total_bytes;
                     let progress = process.copied_bytes as f64 / process.total_bytes as f64;
                     info!("{}: {}", path.display(), progress);
+                    let app = app_inner.clone();
+                    slint::invoke_from_event_loop(move || {
+                        let app = app.unwrap();
+                        app.global::<SetupPageLogic>()
+                            .set_copied_thing(path.to_string_lossy().to_string().into());
+                        app.global::<SetupPageLogic>().set_progress(progress as f32);
+                    })
+                    .expect("Slint main loop should be running");
                     TransitProcessResult::ContinueOrAbort
                 },
             );
             match result {
-                Ok(written) => {
+                Ok(written) if written >= total_bytes => {
                     info!(
                         "Successfully copied {written} bytes from {:?} to {canon_dest}",
                         sources.map(|s| s.display().to_string()),
-                    )
+                    );
+                    slint::invoke_from_event_loop(move || {
+                        let app = app.unwrap();
+                        app.global::<SetupPageLogic>().set_copied_thing("".into());
+                        app.set_setup_done(true);
+                        app.set_current_item(1);
+                    })
+                    .expect("Slint main loop should be running");
                 }
-                Err(e) => error!(
-                    "Failed to copy {:?} to {canon_dest}: {e:?}",
-                    sources.map(|s| s.display().to_string()),
-                ),
+                Ok(written) => {
+                    error!(
+                        "Failed to copy {:?} to {canon_dest}: only wrote {written} bytes instead of {total_bytes}",
+                        sources.map(|s| s.display().to_string()),
+                    );
+                    slint::invoke_from_event_loop(move || {
+                        let app = app.unwrap();
+                        app.global::<SetupPageLogic>().set_copied_thing("".into());
+                    })
+                    .expect("Slint main loop should be running");
+                }
+                Err(e) => {
+                    error!(
+                        "Failed to copy {:?} to {canon_dest}: {e:?}",
+                        sources.map(|s| s.display().to_string()),
+                    );
+                    slint::invoke_from_event_loop(move || {
+                        let app = app.unwrap();
+                        app.global::<SetupPageLogic>().set_copied_thing("".into());
+                    })
+                    .expect("Slint main loop should be running");
+                }
             };
-        })
-        .expect("Slint process should be ready by now");
+        });
     }
 }
 
